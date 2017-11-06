d6a5339f3bf1cdf3e38b82f044c8b232db37806d
David Baker <d.baker@vlkextern.nl>
2017-11-06 12:42:43 +0100

    add sync

--- app/sw.js
+++ app/sw.js
@@ -47,3 +47,29 @@ self.addEventListener('install', function(event){
         } 
     
     });
+
+    self.addEventListener('sync', function(event) {
+      fetch('https://baconipsum.com/api/?type=all-meat&sentences=1&start-with-lorem=1')
+      .then(
+        function(response) {
+          if (response.status !== 200) {
+            console.log('Looks like there was a problem. Status Code: ' +
+              response.status);
+            return;
+          }
+      
+          // Examine the text in the response
+          response.json().then(function(data) {
+            const title = 'Hey! Look!';
+                  const options = {
+                    body: data[0],
+                    icon: 'images/navi.jpg'
+                  };
+                   event.waitUntil(self.registration.showNotification(title, options));
+          });
+        }
+      )
+      .catch(function(err) {
+        console.log('Fetch Error :-S', err);
+      });
+    });
